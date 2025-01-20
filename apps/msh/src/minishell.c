53dc4e8828bab8b58d304e377dd60b288d093bb5
ghassen-hafsia <ghassen.hafsia@insat.ucar.tn>
2025-01-20 18:12:43 +0100

    handle_env_variable_PS1:this function is to check if the input is special variable PS1 trim_spaces:removing the part of leading spaces because we need this process with handle_env_variable_PS1

--- apps/msh/src/minishell.c
+++ apps/msh/src/minishell.c
@@ -1,40 +1,60 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 void trim_spaces(char *buffer); 
+bool handle_env_variable_PS1(char *buffer); 
 
 int main(void){
-	
+
 	char buffer[1024]; 
 
 	while(1){
 		printf("miniShell@ghassen-hafsia>"); 
 		fgets(buffer, sizeof(buffer), stdin); 
 		trim_spaces(buffer);
-		// printf("test :%s\n", buffer);  
 	}
 }	
 
+bool handle_env_variable_PS1(char *buffer){
 
-void trim_spaces(char *buffer){
-	
-	int start = 0;
-	int end = strlen(buffer) - 1; 
+	char variable[20], value[20]; 
+	int start = 0; 
 
 	while (buffer[start] == ' '){
 		start++; 
 	}
+	int j = 0; 
 
-	while ((end > start) && (buffer[end] == ' ')){
+	memmove(buffer, buffer + start, strlen(buffer) - start +1); 
+
+	char *equal = strchr(buffer, '='); 
+	if (equal == NULL) return false; 
+	if ((*(equal + 1) == ' ') || (*(equal - 1) == ' ')) return false; 
+	
+	*equal = '\0'; 
+	strncpy(variable, buffer, sizeof(variable) - 1); 
+	variable[sizeof(variable) - 1] = '\0'; 
+	strncpy(value, equal +1 ,sizeof(value) - 1); 
+	value[sizeof(value) - 1] = '\0'; 
+	
+	if (strncmp(variable, "PS1\0", 4) == 0) return true;
+	
+	return false; 
+} 
+
+void trim_spaces(char *buffer){
+	
+	int end = strlen(buffer) - 1; 
+
+
+	while ((end) && (buffer[end] == ' ')){
 		end --; 
 	}
 	int j = 0; 
 
-	for (int i = start ; i < end; i++){
-		buffer[j++] = buffer[i]; 
-	}
+	buffer[end + 1] = '\0';
 
-	buffer[j] = '\0';
 	j = 0;
 	int space_found = 0;  
 	for (int i = 0; buffer[i] != '\0'; i++) {
